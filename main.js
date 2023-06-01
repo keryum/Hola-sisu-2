9769428532f4b0797c9a2b34426b83f60c3e5118
keryum <alejandro_suarezp@outlook.com>
2023-05-31 23:02:32 -0500

    .

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const servicios = document.querySelector('#servicios')
 const nosotros = document.querySelector('#quienes-somos')
 const metodologia = document.querySelector('#como-trabajamos')
 const contacto = document.querySelector('#contactanos')
+const mobileNavbarCloseBtn = document.querySelector('.mobile-navbar-close-btn')
+const mobileNavbarOpenBtn = document.querySelector('.mobile-navbar-open-btn')
+const mobileNavbar = document.querySelector('.mobile-navbar')
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
 const slider = document.querySelector('#servicios-cards-container')
@@ -15,8 +18,10 @@ const slider = document.querySelector('#servicios-cards-container')
 inicioNav.addEventListener('click', scrollToInicio);
 serviciosNav.addEventListener('click', scrollToServicios);
 nosotrosNav.addEventListener('click', scrollToNosotros);
-metodologiaNav.addEventListener('click', scrollToMetodologia)
-contactoNav.addEventListener('click', scrollToContacto)
+metodologiaNav.addEventListener('click', scrollToMetodologia);
+contactoNav.addEventListener('click', scrollToContacto);
+mobileNavbarOpenBtn.addEventListener('click', showMobileNavbar);
+mobileNavbarCloseBtn.addEventListener('click', closeMobileNavbar);
 prev.addEventListener('click', () => {
     slider.scrollLeft -=300
 });
@@ -24,22 +29,35 @@ next.addEventListener('click', () => {
     slider.scrollLeft +=300
 });
 
+function showMobileNavbar() {
+    mobileNavbar.classList.toggle('inactive')
+}
+
+function closeMobileNavbar() {
+    mobileNavbar.classList.add('inactive')
+}
+
 function scrollToServicios() {
     servicios.scrollIntoView()
+    closeMobileNavbar()
 }
 
 function scrollToInicio() {
     inicio.scrollIntoView()
+    closeMobileNavbar()
 }
 
 function scrollToNosotros() {
     nosotros.scrollIntoView()
+    closeMobileNavbar()
 }
 
 function scrollToMetodologia() {
     metodologia.scrollIntoView()
+    closeMobileNavbar()
 }
 
 function scrollToContacto() {
     contacto.scrollIntoView({block:'center'})
+    closeMobileNavbar()
 }
